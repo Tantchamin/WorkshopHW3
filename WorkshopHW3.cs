a8423ee5502607b1d5aaef621aeff01c1f19aee7
Tantchamin <tantchamin@gmail.com>
2021-03-06 22:07:19 +0700

    Workshop_HW3

---
+++ WorkshopHW3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WorkshopHW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double p = Math.Pow(x - 1, 2);
+            double t = Math.Sqrt(y) + 1;
+
+            if (mode == "time" || mode == "price")
+            {
+                if (x < 0)
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+                else
+                {
+                    if (mode == "time")
+                    {
+                        Console.WriteLine("{0}, {1}", x, p);
+                    }
+                    if (mode == "price")
+                    {
+                        Console.WriteLine("{0}, {1}", t, y);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid mode");
+                if (x < 0)
+                {
+                    Console.WriteLine("Invalid mouse position");
+                }
+            }
+        }
+    }
+}
